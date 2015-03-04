e729bea1b0eb8112ebf5b985ac6d2e7aa4f6c8eb
gimarinelli <giacomo.marinelli.97@hotmail.it>
2015-03-04 16:50:04 +0100

    Create operazioni.sh

---
+++ operazioni.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+x=5
+y=3
+somma=$(($x + $y))
+sottrazione=$(($x - $y))
+moltiplicazione=$(($x * $y))
+divisione=$(($x / $y))
+echo "Somma: $somma"
+echo "Sottrazione: $sottrazione"
+echo "Moltiplicazione: $moltiplicazione"
+echo "Divisione: $divisione"
+
